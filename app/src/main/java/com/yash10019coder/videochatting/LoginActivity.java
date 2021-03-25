9b0d8d2d4fb7b431105df90f91b20103f5e8285c
yash10019coder <lit2020066@iiitl.ac.in>
2021-03-25 23:50:29 +0530

    Fixed Login SignUp and main Activity

--- app/src/main/java/com/yash10019coder/videochatting/LoginActivity.java
+++ app/src/main/java/com/yash10019coder/videochatting/LoginActivity.java
@@ -2,17 +2,33 @@ package com.yash10019coder.videochatting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.yash10019coder.videochatting.databinding.ActivityLoginBinding;
+
+import static java.lang.Character.isDigit;
 
 public class LoginActivity extends AppCompatActivity {
 
     ActivityLoginBinding binding;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        String email=binding.etEmail.getText().toString().trim();
+        String password=binding.etPassword.getText().toString().trim();
+        auth=FirebaseAuth.getInstance();
+        binding.btsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,SignUp.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/yash10019coder/videochatting/SignUp.java
+++ app/src/main/java/com/yash10019coder/videochatting/SignUp.java
@@ -2,13 +2,25 @@ package com.yash10019coder.videochatting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.yash10019coder.videochatting.databinding.ActivityLoginBinding;
+import com.yash10019coder.videochatting.databinding.ActivitySignUpBinding;
 
 public class SignUp extends AppCompatActivity {
-
+    ActivitySignUpBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding=ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUp.this,LoginActivity.class));
+            }
+        });
     }
 }
