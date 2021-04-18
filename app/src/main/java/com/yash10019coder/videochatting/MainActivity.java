7b6c78d37ed3283c36c479ca18b05fe7a4a6eac3
yash10019coder <lit2020066@iiitl.ac.in>
2021-04-18 22:15:21 +0530

    Revert "Added database and fixed google sign in and other bugs"
    This reverts commit 41835dfe17d77f7178dac1cd7bfc642d8d4b6653.

--- app/src/main/java/com/yash10019coder/videochatting/MainActivity.java
+++ app/src/main/java/com/yash10019coder/videochatting/MainActivity.java
@@ -1,75 +1,29 @@
 package com.yash10019coder.videochatting;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
-import android.view.View;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
 import com.yash10019coder.videochatting.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
-    FirebaseAuth mAuth;
-    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        mAuth=FirebaseAuth.getInstance();
 //        startActivity(new Intent(MainActivity.this,LoginActivity.class));
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//
-//                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-//            }
-//        }, 2000);
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-        .requestEmail()
-        .build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        binding.button.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
+            public void run() {
 
-                Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
-                signOut();
-                startActivity(intent);
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
-        });
+        }, 2000);
     }
-    private void signOut() {
-    mGoogleSignInClient.signOut()
-            .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    // ...
-                }
-            });
-}
 }
--- app/src/main/java/com/yash10019coder/videochatting/SignUp.java
+++ app/src/main/java/com/yash10019coder/videochatting/SignUp.java
@@ -1,105 +1,25 @@
 package com.yash10019coder.videochatting;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.yash10019coder.videochatting.databinding.ActivityLoginBinding;
 import com.yash10019coder.videochatting.databinding.ActivitySignUpBinding;
-import com.yash10019coder.videochatting.models.users;
 
 public class SignUp extends AppCompatActivity {
     ActivitySignUpBinding binding;
-    FirebaseAuth mAuth;
-    ProgressDialog progressDialog;
-    String TAG = "TAG";
-    private FirebaseFirestore db;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        Log.d(TAG, "onCreate: signup");
-        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        binding=ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        mAuth = FirebaseAuth.getInstance();
-        db= FirebaseFirestore.getInstance();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-//        progressDialog.setTitle("Logging in");
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser != null ) {
-//            progressDialog.show();
-            startActivity(new Intent(SignUp.this, MainActivity.class));
-//            progressDialog.dismiss();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-//        progressDialog.setTitle("Sign Up");
-//        progressDialog.setMessage("Creating your Account");
         binding.btlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SignUp.this, LoginActivity.class));
-            }
-        });
-        binding.btSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                progressDialog.show();
-                String name = binding.etUsername.getText().toString().trim();
-                String email = binding.etEmail.getText().toString().trim();
-                String password = binding.etPassword.getText().toString().trim();
-                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        progressDialog.dismiss();
-                        if (task.isSuccessful()) {
-                            users users1=new users();
-                            users1.setUsername(binding.etUsername.getText().toString());
-                            users1.setId(mAuth.getUid());
-                            users1.setMail(email);
-                            users1.setUser_password(password);
-                            db.collection("users").add(users1).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                                @Override
-                                public void onSuccess(DocumentReference documentReference) {
-                                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                                }
-                            }).addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Log.w(TAG, "Error adding document", e);
-                                }
-                            });
-                            startActivity(new Intent(SignUp.this, MainActivity.class));
-                            Toast.makeText(SignUp.this, "Your Account is created.", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(SignUp.this, task.getException().getLocalizedMessage(),
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+                startActivity(new Intent(SignUp.this,LoginActivity.class));
             }
         });
     }
