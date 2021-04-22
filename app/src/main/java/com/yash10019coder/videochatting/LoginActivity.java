3877dafec2e71792e5c1e7e2f0c557200d3925ec
yash10019coder <lit2020066@iiitl.ac.in>
2021-04-22 12:36:52 +0530

    Added Crashlytics and performance monitoring libs Added Splash Screen Added icon now app is redy to upload in play store

--- app/src/main/java/com/yash10019coder/videochatting/LoginActivity.java
+++ app/src/main/java/com/yash10019coder/videochatting/LoginActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -52,7 +53,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        Log.d(TAG, "onCreate: login");
+
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         mAuth = FirebaseAuth.getInstance();
